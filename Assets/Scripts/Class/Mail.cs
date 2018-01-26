6c1fb5e80430b244159f2a6bcc933e3257906237
GregLiu <hkllc2014@gmail.com>
2018-01-27 02:31:39 +0800

    Area&Mail classes

---
+++ Assets/Scripts/Class/Mail.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mail {
+
+    private float pos_x; // x 
+    private float pos_y; // y 
+    private float type; // type of the mail
+    private float value; // normal return value
+    private float max_value; // max return value
+    private float time; // exist time
+
+
+    //
+    //  1. Get x/y from area
+    //  2. Get random type and related value
+    //  3. Determine the time left
+    //
+    public void CreateNewMail(float x, float y) {
+        pos_x = x;
+        pos_y = y;
+        type = 1;
+        value = 10;
+        max_value = 20;
+        time = 60 * 1000;
+    }
+
+    //
+    //  Mail Destroy
+    //
+    public void DestroyMail() {
+
+    }
+
+    //
+    //  Move mail to the closest postrans
+    //
+    public void MoveMail(MailArea[] mailAreaArr, Vector2 moveDir) {
+
+    }
+
+
+}
---
+++ Assets/Scripts/Class/MailArea.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MailArea {
+
+    private float pos_x; // x of center
+    private float pos_y; // y of center
+    private float radius; // radius of the circle
+    private float generate_speed; // control the speed of generation, controled by sin@
+    private int max_mail; // maximum hold of mails in area
+    private int total_mail; // history record of total mail generated
+        
+    //
+    //  1. Detect other areas 
+    //  2. Generate random x/y/r
+    //
+    public void CreateNewArea(MailArea[] mailAreaArr) {
+            
+    }
+
+    //
+    //  Star the mail generation in a changing speed (sin)
+    //
+    public void StartGenerateMail() {
+
+    }
+
+    //
+    //  Generate one mail in the area
+    //  @mail_x
+    //  @mail_y
+    //  @mail_type
+    //
+    private void GenerateOneMail() {
+
+    }
+
+    //
+    //  Stop the mail gerneration
+    //
+    public void StopGenerateMail() {
+
+    }
+   
+}
