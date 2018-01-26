05e87818e8ecea22dc101a8e5b10747f5ba2b8fa
Houdou <houdou.pt@gmail.com>
2018-01-27 00:31:29 +0800

    GameMaster

--- Assets/Scripts/Managers/GameMaster.cs
+++ Assets/Scripts/Managers/GameMaster.cs
@@ -1,16 +1,77 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
-public class GameMaster : MonoBehaviour {
+public class GameMaster : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    #region Singleton
+    private static GameMaster _instance;
+    private static object _lock = new object();
+
+    public static GameMaster Instance
+    {
+        get
+        {
+            if(applicationIsQuitting) {
+                Debug.LogWarning("[Singleton] Instance '" + typeof(GameMaster) +
+                                 "' already destroyed on application quit. Returning null.");
+                return null;
+            }
+
+            lock(_lock) {
+                if(_instance == null) {
+                    _instance = (GameMaster)FindObjectOfType(typeof(GameMaster));
+
+                    if(_instance == null) {
+                        GameObject singleton = new GameObject("GameMaster");
+                        _instance = singleton.AddComponent<GameMaster>();
+
+                        DontDestroyOnLoad(singleton);
+
+                        Debug.Log("[Singleton] Instance '" + typeof(GameMaster) +
+                                  "' is created.");
+                    }
+                }
+
+                return _instance;
+            }
+        }
+    }
+
+    private static bool applicationIsQuitting = false;
+
+    public void OnDestroy()
+    {
+        applicationIsQuitting = true;
+    }
+    #endregion
+
+    public bool useScreenLogger = true;
+
+
+    public Text ScreenLogger { get; private set; }
+
+    // Use this for initialization
+    void Awake()
+    {
+        ScreenLogger = GameObject.Find("ScreenLogger").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public string ScreenLoggerHeader = "DEBUG:\n";
+
+    public void Log(object any)
+    {
+        if(useScreenLogger) {
+            ScreenLogger.text = ScreenLoggerHeader + any.ToString();
+        } else {
+            Debug.Log(any);
+        }
+    }
 }
