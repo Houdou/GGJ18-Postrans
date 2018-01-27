00340dff4155cb75930ff85d618a7a3f2107c4d5
Butter-89 <906672365@qq.com>
2018-01-27 14:35:37 +0800

    UnreliableMonryControl

---
+++ Assets/Scripts/TotalMoney.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+    private int totalMoney;
+    public Text moneyUI;
+	// Use this for initialization
+	void Start () {
+        totalMoney = 500;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("Total money: "+totalMoney);
+	}
+
+    public void income(int a)
+    {
+        totalMoney += a;
+    }
+    public void expense(int a)
+    {
+        totalMoney -= a;
+    }
+    void SetText()
+    {
+        moneyUI.text = "Total: " + totalMoney.ToString();
+    }
+}
