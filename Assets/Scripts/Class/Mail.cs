c20900635a243520a5f990ab68d53ad31c911f86
GregLiu <hkllc2014@gmail.com>
2018-01-27 16:22:20 +0800

    mail

--- Assets/Scripts/Class/Mail.cs
+++ Assets/Scripts/Class/Mail.cs
@@ -2,41 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mail {
+public class Mail : MonoBehaviour {
+    public float type; // type of the mail
+    public float value; // normal return value
+    public float max_value; // max return value
+    public float rate; // rate to generate
 
     private float pos_x; // x 
     private float pos_y; // y 
-    private float type; // type of the mail
-    private float value; // normal return value
-    private float max_value; // max return value
-    private float time; // exist time
+    private float time = 0.0f; // time
+    private float exsit_time = 30.0f; // exist time
 
+    // Use this for initialization
+    void Start() {
+        MailManager MailManager = GameObject.Find("GameMaster").GetComponent<MailManager>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        time += Time.deltaTime;
+    }
 
     //
     //  1. Get x/y from area
     //  2. Get random type and related value
     //  3. Determine the time left
     //
-    public void CreateNewMail(float x, float y) {
-        pos_x = x;
-        pos_y = y;
-        type = 1;
-        value = 10;
-        max_value = 20;
-        time = 60 * 1000;
+    public void MailInit(Vector2 init_pos) {
+        Vector3 pos = new Vector3(init_pos.x, init_pos.y, 0);
+        this.transform.position = pos;
+
+        pos_x = init_pos.x;
+        pos_y = init_pos.y;
+
     }
 
     //
     //  Mail Destroy
     //
-    public void DestroyMail() {
-
+    void DestroyMail() {
+        Object.Destroy(this);
     }
 
     //
     //  Move mail to the closest postrans
     //
-    public void MoveMail(MailArea[] mailAreaArr, Vector2 moveDir) {
+    void MoveMail(Vector2 moveDir) {
 
     }
 
---
+++ Assets/Scripts/Managers/MailManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MailManager : MonoBehaviour {
+    public GameObject map_bg;
+    public GameObject MailArea;
+    public List<GameObject> MailAreas = new List<GameObject>();
+    public List<GameObject> MailStation = new List<GameObject>();
+    private float time = 0.0f; // time count since start
+
+
+	// Use this for initialization
+	void Start () {
+        Vector2 bound = new Vector2(map_bg.GetComponent<Renderer>().bounds.size.x / 2, map_bg.GetComponent<Renderer>().bounds.size.y / 2);
+
+        CreateNewArea(new Vector2(1f, 1f), bound);
+        CreateNewArea(new Vector2(-3f, -3f), bound);
+        CreateNewArea(new Vector2(6f, -4f), bound);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+    }
+
+    void CreateNewArea(Vector2 init_pos, Vector2 bound) {
+        GameObject area = Object.Instantiate(MailArea);
+        area.GetComponent<MailArea>().AreaInit(init_pos, bound);
+
+        MailAreas.Add(area);
+    }
+}
